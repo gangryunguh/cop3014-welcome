f8316ddd2d7a326f7af802d9db48a27731c8d744
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-08-21 13:25:14 -0500

    Welcome to COP3014 - to test whether the CLion setup completes

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    cout << "Welcome COP3014!" << endl;
+    cout << "\n\nI would like to share the following important announcement which I made in summer 2018." << endl;
+    cout << "\n\nSummer 2018: Important announcement on" << endl;
+    cout << "Significance of Academic Honor Policy" << endl;
+    cout << "...\n\nBefore concluding, I would like to emphasize the" << endl;
+    cout << "significance of Academic Honor Policy which is an important" << endl;
+    cout << "aspect of keeping the integrity and quality of the computer" << endl;
+    cout << "science courses in general. Please don't share your project" << endl;
+    cout << "source with any other students on any occasion. I would like" << endl;
+    cout << "to let you know that, for the summer 2018 COP3014 course, we" << endl;
+    cout << "found several students violated the Academic Honor Policy by" << endl;
+    cout << "sharing their programming assignment source in an advertent" << endl;
+    cout << "and inadvertent manners. Please note that we will run MOSS" << endl;
+    cout << "(plagiarism detection software tool) on future COP3014 student" << endl;
+    cout << "project submissions together with your summer 2018 project" << endl;
+    cout << "submissions. In case your project source code happens to be >%75" << endl;
+    cout << "similar to future student project source (which is statistically" << endl;
+    cout << "almost impossible), your grade will be invalidated and your" << endl;
+    cout << "academic honor code violation will be reported to Dean's office." << endl;
+    cout << "The following is the quick reference for Academic Honor Policy:" << endl;
+    cout << "\nhttps://fda.fsu.edu/sites/g/files/upcbnu636/files/Media/Files/Academic%20Honor%20Policy/AHP%20Quick%20Reference%20Guide%20Nov%202017.pdf"
+         << endl;
+
+    return 0;
+}
